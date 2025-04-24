512c9444dd12c4932a5405deb28197c82f7d5f74
timothyn <tnguyen_42@yahoo.com>
2025-04-24 16:09:25 -0400

    Update Connect-Four

--- js/main.js
+++ js/main.js
@@ -1,13 +1,104 @@
 /*----- constants -----*/
-
+// look-up data structure
+const COLORS = {
+    '1': 'Red',
+    '-1': 'Blue',
+    'null': 'white',
+};
 
 /*----- state variables -----*/
-
+// Define, but do not assign to (initialize)
+let board; // 2D Array / 1/-1 -> player value; null -> cell is empty
+let winner; // null -> no winner or tie, game is in progress; 1/-1 -> the player that won; 'Tie' -> the game has tied
+let turn; // 1/-1 -> the player whose turn it is
 
 /*----- cached elements  -----*/
-
+const msgEl = document.querySelector('h1');
+const playAgainBtn = document.getElementById('play-again');
+const markerEls = [...document.querySelectorAll('#markers > div')]; // Convert the Nodelist into an array
 
 /*----- event listeners -----*/
-
+document.getElementById('markers').addEventListener('click', handleDrop);
 
 /*----- functions -----*/
+init();
+
+// The init function's purpose is to intialize 
+// all state, then call render()
+function init() {
+    // To visualize the mapping (connection) between
+    // the board array and the "cells"/divs in the DOM,
+    // "rotate" the board 90 degrees counter-clockwise
+    board = [
+        [null, null, null, null, null, null], // col 0
+        [null, null, null, null, null, null], // col 1
+        [null, null, null, null, null, null], // col 2
+        [null, null, null, null, null, null], // col 3
+        [null, null, null, null, null, null], // col 4
+        [null, null, null, null, null, null], // col 5
+        [null, null, null, null, null, null], // col 6
+    ];
+    winner = null;
+    turn = 1; // Can we make this random?
+    render();
+};
+
+// In response to user interaction, update all impacted
+// state, then call render()
+function handleDrop(evt) {
+    console.dir(evt.target);
+
+    // 1) Determine the index of the clicked column marker.
+    const colIdx = markerEls.indexOf(evt.target);
+    // 2) If not a valid index, do nothing (return from function).
+    if (colIdx === -1) return;
+    // 3) Create a shortcut variable to the clicked column array, e.g., `colArr`.
+    const colArr = board[colIdx];
+    // 4) Determine the index of the first available "cell" (first `null` element in `colArr`).
+    const rowIdx = colArr.indexOf(null);
+    console.log(rowIdx); // Alt + up/down arrow to move code line around
+    // 5) Update the "cell" in `colArr` with whose turn it is.
+    colArr[rowIdx] = turn;
+    // 6) Compute and update the state of the game (winner?).
+    // 7) Update whose turn it is.
+    turn *= -1;
+    // 8) All state has been updated - call render()!
+    render();
+};
+
+// The purpose of the render() function is to "transfer"/visualize
+// ALL state to/in the DOM
+function render() {
+    renderBoard();
+    renderMessage();
+    renderControls();
+};
+
+function renderControls() {
+    // ternary expression - use when you want to return one of two values
+    // <conditional exp> ? <truthy exp> : <falsy exp>  ? is the ternary operator
+    playAgainBtn.style.visibility = winner ? 'visible' : 'hidden';
+    // TODO: conditionally render the markers
+};
+
+function renderMessage() {
+    if (winner === null) {
+        msgEl.innerHTML = `<span style="color: ${COLORS[turn]}">${COLORS[turn].toUpperCase()}</span>'s Turn`;
+    } else if (winner === 'Tie') {
+        msgEl.innerHTML = "It's a Tie!";
+    } else {
+        // There's a winner!
+        msgEl.innerHTML = `<span style="color: ${COLORS[winner]}">${COLORS[winner].toUpperCase()}</span> Wins!`;
+    }
+};
+
+function renderBoard() {
+    board.forEach((colArr, colIdx) => { // column array and column id
+        colArr.forEach((cellVal, rowIdx) => { // cell value and row id
+            const cellEl = document.getElementById(`c${colIdx}r${rowIdx}`);
+            cellEl.style.backgroundColor = COLORS[cellVal];
+            // cellEl.style.backgroundColor = 'red'; just testing
+        });
+    });
+};
+
